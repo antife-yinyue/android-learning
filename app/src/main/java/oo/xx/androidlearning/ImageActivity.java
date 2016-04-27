860e55c8da377943e2455bd3131057b0c12ab470
尹曰 <junjun.zjj@alipay.com>
2016-04-27 12:30:00 +0800

    Add action bar

--- app/src/main/java/oo/xx/androidlearning/ImageActivity.java
+++ app/src/main/java/oo/xx/androidlearning/ImageActivity.java
@@ -1,8 +1,10 @@
 package oo.xx.androidlearning;
 
-import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -14,10 +16,40 @@ public class ImageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
 
+        setupActionBar();
+
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
         Picasso.with(imageView.getContext())
-            .load("http://cdn.duitang.com/uploads/item/201603/06/20160306115628_dfwuc.jpeg")
-            .into(imageView);
+                .load("http://cdn.duitang.com/uploads/item/201603/06/20160306115628_dfwuc.jpeg")
+                .into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_image, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+            case R.id.action_reload:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/oo/xx/androidlearning/WebviewActivity.java
+++ app/src/main/java/oo/xx/androidlearning/WebviewActivity.java
@@ -1,19 +1,55 @@
 package oo.xx.androidlearning;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class WebviewActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
-        WebView webView = (WebView) findViewById(R.id.webView);
+        setupActionBar();
+
+        webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://baidu.com/");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_webview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+            case R.id.action_reload:
+                webView.reload();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
